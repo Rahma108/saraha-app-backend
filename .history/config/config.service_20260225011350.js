d2341646986e1a695ed85bfdc7ad8c79570ffd99
Rahma108 <salamarahma667@gmail.com>
2026-02-25 01:16:13 +0200

    First-Commit

---
+++ .history/config/config.service_20260225011350.js
@@ -0,0 +1,29 @@
+
+import {resolve} from 'node:path'
+import {config} from 'dotenv'
+
+// لازم اعمل install ===> npm i cross-env 
+export const NODE_ENV = process.env.NODE_ENV
+
+const envPath = {
+    development : `.env.dev`,
+    production :`.env.prod`
+}
+
+config({path:resolve(`./config/${envPath[NODE_ENV]}`)})
+export const port = process.env.PORT ?? 7000
+
+export const DB_URI = process.env.DB_URI
+
+
+export const SALT_ROUND= parseInt(process.env.SALT_ROUND ?? '10') 
+export const IV_LENGTH = parseInt(process.env.IV_LENGTH ?? '16')
+
+export const SECURITY_KEY = process.env.SECURITY_KEY
+
+export const System_TOKEN_SECRET_KEY= process.env.System_TOKEN_SECRET_KEY
+export const User_TOKEN_SECRET_KEY= process.env.User_TOKEN_SECRET_KEY
+
+export const System_REFRESH_TOKEN_SECRET_KEY= process.env.System_REFRESH_TOKEN_SECRET_KEY
+export const User_REFRESH_TOKEN_SECRET_KEY= process.env.User_REFRESH_TOKEN_SECRET_KEY
+
---
+++ src/modules/index.js
@@ -0,0 +1,10 @@
+// export ..................................
+export * from './auth/index.js'
+export *  from './user/index.js'
+export *  from './otp/index.js'
+
+
+
+
+
+
