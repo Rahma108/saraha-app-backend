a18a700314e7f12fd6b026794296cfc5e16c5199
Rahma108 <salamarahma667@gmail.com>
2026-02-28 16:00:17 +0200

    fix error handling

--- .history/src/common/utils/response/error.response_20260225014414.js
+++ .history/src/common/utils/response/error.response_20260225014414.js
@@ -1,3 +1,4 @@
+
 import { NODE_ENV } from "../../../../config/config.service.js"
 
 export const GlobalError =(error ,  req , res , next )=>{
@@ -17,17 +18,17 @@ export const ErrorException = ({message='Fail' , cause=undefined } = {} )=>{
     throw new Error( message , {cause} )
 }
 export const BadRequestException = ({ message='BadRequestException' , extra = undefined }  = {} )=>{
-    return ErrorException({message , cause:{status:400 ,   extra} })
+    return ErrorException({message , cause:{status:400} , extra })
 }
 export const UnauthorizedException = ({ message='UnauthorizedException' , extra =undefined } = {} )=>{
-    return ErrorException({message , cause:{status:401 , extra}  })
+    return ErrorException({message , cause:{status:401} , extra })
 }
 export const forbiddenException = ({message='forbiddenException' , extra = undefined } = {} )=>{
-    return ErrorException({message , cause:{status:403 , extra}  })
+    return ErrorException({message , cause:{status:403} , extra })
 }
 export const NotFoundException = ({ message='NotFoundException' , extra ={undefined} }= {} )=>{
-    return ErrorException({message , cause:{status:404  , extra  }})
+    return ErrorException({message , cause:{status:404} , extra })
 }
 export const ConflictException = ({message='ConflictException' , extra = undefined} = {} )=>{
-    throw ErrorException({message , cause:{status:409  , extra} })
+    return ErrorException({message , cause:{status:409} , extra })
 }
